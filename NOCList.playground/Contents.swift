fc633436214a19f8bcfd59054accaa1bbb17654d
Ahava Morse <ahavamorse@icloud.com>
2020-03-17 13:48:24 -0700

    Step 9: function that finds the number of each level agent and prints it out

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -100,9 +100,24 @@ func findHighRisk() {
 findHighRisk()
 //: ## Step 9
 //: Create a function that finds totals for low, mid, and high level agents. Low level agents are 4 or lower, mid are 5-7, and high level agents are 8 or above. Iterate over each agent and use a `switch` statement to determine their level group. At the end of the function, print a statement like the following: "# low level agents, # mid level agents, and # high level agents"
-
-
-
+func numberOfAgentsInLevels() {
+    
+    var lowLevelAgents: Int = 0
+    var midLevelAgents: Int = 0
+    var highLevelAgents: Int = 0
+    
+    for agent in agents {
+        switch agent.accessLevel {
+        case 1,2,3,4:
+            lowLevelAgents += 1
+        case 5,6,7:
+            midLevelAgents += 1
+        default:
+            highLevelAgents += 1
+        }
+    }
+    print("\(lowLevelAgents) low level agents, \(midLevelAgents) mid level agents, and \(highLevelAgents) high level agents")
+}
 //: ## Step 10
 //: Call the above function and check its output in the console.
 
